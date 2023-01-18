c994b03eaaf03ee31e238986c5331a1d12a31250
Sravan T P <sravantp03@gmail.com>
2023-01-18 12:35:25 +0530

    Added new Search Component + Cleaned App.tsx

---
+++ src/components/Search.tsx
@@ -0,0 +1,43 @@
+import { ChangeEvent } from 'react';
+import { optionType } from "../types";
+
+// Defining types for Props.
+type Props = {
+    term: string,
+    options: [],
+    city: optionType | null,
+    onInputChange: (e: ChangeEvent<HTMLInputElement>) => void,
+    onOptionSelect: (option: optionType) => void,
+    fetchWeather: (city: optionType) => void
+  }
+
+const Search = ({term, city, options, onInputChange, onOptionSelect, fetchWeather}: Props): JSX.Element => {
+  return (
+    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+      <section className="bg-white rounded bg-opacity-20 backdrop-blur-lg drop-shadow-lg w-full md:max-w-[500px] p-4 flex flex-col text-center justify-center md:px-10 lg:p-24 h-full sm:h-[500px] lg:h-[500px] text-zinc-700">
+        <h1 className="text-4xl font-thin">Weather <span className="font-black">Forecast</span></h1>
+        <p className="text-sm mt-3 mb-5">Enter below a place you want to know the weather of and select an option from dropdown</p>
+        <div className='relative flex mt-4 justify-center'>
+          <input type="text" value={term} onChange={onInputChange} className='px-2 py-1 rounded-l-md border-2 border-white outline-none text-black' />
+          <ul className='bg-white rounded-b-md top-9 absolute ml-1 left-16 sm:left-20 md:left-8'>
+            {
+              options.map((option: optionType, index: number) => (
+                <li key={index}>
+                  <button className='text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1' onClick={() => onOptionSelect(option)}>{option.name}, {option.country}</button>
+                </li>
+              ))
+            }
+          </ul>
+          <button className="rounded-r-md text-zinc-100 border-2 border-zinc-100 px-2 py-1 cursor-pointer hover:text-zinc-500" onClick={()=> {
+            if (!city) {
+              return;
+            }
+            fetchWeather(city);
+          }}>search</button>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default Search;
